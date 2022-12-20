bac264193c9b0d9b2d5807e0fa060951f97b9b01
florence <idara.ed@gmail.com>
2022-12-20 21:55:19 +0000

    new commit

---
+++ app.java
@@ -0,0 +1,5 @@
+  Develops, Tests, Builds, qualification, backup   
+   deploys & monitors applications 
+  Applications are the output from Softwares development 
+4-Developers: Landmark have a team of developers, 4 developers are assigned their task is to make sure the customers are satisfied.
+
